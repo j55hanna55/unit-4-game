3d2f5da5aeb95fc03da7065fa34be35cdbbde23c
Jacob Hanna <jacob@Jacobs-MacBook-Pro.local>
2019-07-16 10:42:49 -0700

    Creating my functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//     var wins = 0;
+// 	var losses = 0;
+// 	var total = 0;
+	
+
+// 	$('#wins').text(wins);
+// 	$('#losses').text(losses);
+// 	$('#total').text(total);
+// 	$('#rn').text(rn);
+
+	
+	
+
+// document.getElementById("rn").innerHTML =
+// Math.floor(Math.random() * 101) + 19;
+
+
+// $("#13").click(function(){
+// 	total = total + 13;
+//     $('#total').text(total);
+// });
+
+
+
+// $("#10").click(function(){
+//   total = total + 10;
+//     $('#total').text(total);
+// });
+
+
+// $("#7").click(function(){
+//   total = total + 7;
+//     $('#total').text(total);
+// });
+
+
+// $("#1").click(function(){
+//   total = total + 1;
+//     $('#total').text(total);
+// });
+
+
+
+
+// var rn = $('#rn').text();
+
+// if (total > rn){
+// 			losses++;
+// 			$('#losses').text(losses);
+// 		}
