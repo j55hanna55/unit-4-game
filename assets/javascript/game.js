0aea7ac8ce3ca8031555b86d3d8c00bd7dadb804
Jacob Hanna <jacob@Jacobs-MacBook-Pro.local>
2019-07-19 15:02:10 -0700

    fixing my link to css file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,61 @@
-//     var wins = 0;
-// 	var losses = 0;
-// 	var total = 0;
+    var wins = 0;
+	var losses = 0;
+	var total = 0;
 	
 
-// 	$('#wins').text(wins);
-// 	$('#losses').text(losses);
-// 	$('#total').text(total);
-// 	$('#rn').text(rn);
+	$('#wins').text(wins);
+	$('#losses').text(losses);
+	$('#total').text(total);
+	$('#rn').text(rn);
+
 
 	
-	
 
-// document.getElementById("rn").innerHTML =
-// Math.floor(Math.random() * 101) + 19;
-
-
-// $("#13").click(function(){
-// 	total = total + 13;
-//     $('#total').text(total);
-// });
+document.getElementById("rn").innerHTML =
+Math.floor(Math.random() * 101) + 19;
 
 
-
-// $("#10").click(function(){
-//   total = total + 10;
-//     $('#total').text(total);
-// });
-
-
-// $("#7").click(function(){
-//   total = total + 7;
-//     $('#total').text(total);
-// });
+$("#13").click(function(){
+	total = total + 13;
+    $('#total').text(total);
+});
 
 
-// $("#1").click(function(){
-//   total = total + 1;
-//     $('#total').text(total);
-// });
+
+$("#10").click(function(){
+  total = total + 10;
+    $('#total').text(total);
+});
 
 
+$("#7").click(function(){
+  total = total + 7;
+    $('#total').text(total);
+});
+
+
+$("#1").click(function(){
+  total = total + 1;
+    $('#total').text(total);
+});
+
+
+
+
+// function checkGameStatus() {
+//    var currentScore = $("#rn").text()
+//    currentScore = parseInt(currentScore);
+//    var targetScore = $("#total").text()
+//    targetScore = parseInt(targetScore);
+// }
+
+// if (currentScore == targetScore){
+// 	var targetScore = $("#wins").text();
+// 	targetScore = parseInt(targetScore);
+// 	wins++;
+// 	alert("nsoji");
+// 	console.log(targetScore);
+// }
 
 
 // var rn = $('#rn').text();
@@ -48,3 +64,43 @@
 // 			losses++;
 // 			$('#losses').text(losses);
 // 		}
+
+// function checkGameStatus { 
+// 	var currentScore = $("#rn").text()
+// 	currentScore = parseInt(currentScore);
+
+// 	var targetScore = $("#total").text()
+// 	targetScore = parseInt(targetScore);
+// }
+
+// 		if (currentScore == targetScore) {
+//         var winnerNum = $("#wins").text()
+//         winnerNum = parseInt(winnerNum);
+//         winnerNum++;
+//         $("#wins").text(winnerNum);
+//         reset();
+
+//     }
+//     else if (currentScore > targetScore) {
+//         var lossesNum = $("#losses").text()
+//         lossesNum = parseInt(lossesNum);
+//         lossesNum++;
+//         $("#losses").text(lossesNum);
+//         reset();
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
